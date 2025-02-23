ef38a6f15fae4a23b60bf3746a322338875f66dc
Caliperti1 <christopher.aliperti@gmail.com>
2025-02-23 10:48:01 -0500

    Moved last years code to archive and started 2025 program

---
+++ DataManager.m
@@ -0,0 +1,14 @@
+%% DataManager
+% This will read all of the necessary data out of the data folder (Updated
+% yearly from Kaggle's March Machine Learning Madness challenge)
+
+%% Tournament Seeds Key 
+TournamentSeeds = readtable("2024_tourney_seeds.csv");
+TournamentSeeds.Seed = string(TournamentSeeds.Seed);
+TournamentSeeds.Tournament = string(TournamentSeeds.Tournament);
+TournamentSeeds = TournamentSeeds(TournamentSeeds.Tournament == "M",:);
+ % Tournament (M or W) // Seed // Team ID (Key for other tables)
+
+%% Raw Data 
+
+RawData = [];
---
+++ _Archive_2024/MM24_MonteCarlo_SimScript.m
@@ -0,0 +1,22 @@
+%% March Madness Monte Carlo 
+clc 
+close all
+clear 
+
+%% Load variables from pre pre-processesing 
+
+% Declare global variables for func
+
+%% Run loop 
+
+% turn other monte carlo into function (takes 5 numbers as inputs)
+
+% each time throuhg loop it renadomly genreates numbers to pass to each
+% layer of function that chooses whihc models will be averaged to pick
+% winners 
+
+% Add braket points from each iteration to matrix 
+
+% sum all bracket points 
+
+% Generate a winning brakcet basedon bracket challenge scores as metric 
